4670e6dbaee519c0c0828d1d322f7746e186d8c7
Nabihakhawar <nabihakhawar54@gmail.com>
2026-05-14 22:51:16 +0500

    Player

--- src/main/java/com/example/pacman/Player.java
+++ src/main/java/com/example/pacman/Player.java
@@ -1,8 +1,10 @@
+
 package com.example.pacman;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyCode;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
 
 public class Player extends Entity {
 
@@ -12,13 +14,17 @@ public class Player extends Entity {
     private int row;
     private int col;
 
+    // mouth animation
+    private double mouthAngle = 60;
+    private boolean closing = true;
+
     public Player(Maze maze, CollisionSystem collision, double x, double y) {
+
         super(maze, collision);
 
         speed = 2;
         radius = maze.TILE_SIZE * 0.35;
 
-        // IMPORTANT: use Entity.x / Entity.y (NOT new variables)
         this.x = x;
         this.y = y;
 
@@ -46,12 +52,15 @@ public class Player extends Entity {
 
         currentDir = "";
         nextDir = "";
+
+        mouthAngle = 65;
+        closing = true;
     }
 
     @Override
     public void update() {
 
-        // direction buffer
+        // buffered direction
         if (!nextDir.isEmpty() && canMove(nextDir)) {
             currentDir = nextDir;
         }
@@ -60,25 +69,67 @@ public class Player extends Entity {
         double tempY = y;
 
         // movement
-        if (currentDir.equals("LEFT"))  tempX -= speed;
-        if (currentDir.equals("RIGHT")) tempX += speed;
-        if (currentDir.equals("UP"))    tempY -= speed;
-        if (currentDir.equals("DOWN"))  tempY += speed;
+        switch (currentDir) {
+
+            case "LEFT":
+                tempX -= speed;
+                break;
+
+            case "RIGHT":
+                tempX += speed;
+                break;
+
+            case "UP":
+                tempY -= speed;
+                break;
+
+            case "DOWN":
+                tempY += speed;
+                break;
+        }
 
         // collision
         if (collision.canMove(tempX, tempY, radius)) {
+
             x = tempX;
             y = tempY;
+
+        } else {
+
+            // stop when hitting wall
+            currentDir = "";
         }
 
-        // snap to grid
+        // update row/col continuously
+        row = (int)(y / maze.TILE_SIZE);
+        col = (int)(x / maze.TILE_SIZE);
+
+        // snap to center
         if (isCentered()) {
 
             x = col * maze.TILE_SIZE + maze.TILE_SIZE / 2.0;
             y = row * maze.TILE_SIZE + maze.TILE_SIZE / 2.0;
+        }
 
-            row = (int)(y / maze.TILE_SIZE);
-            col = (int)(x / maze.TILE_SIZE);
+        // mouth animation
+        if (!currentDir.isEmpty()) {
+
+            if (closing) {
+
+                mouthAngle -= 2;
+
+                if (mouthAngle <= 10) {
+                    closing = false;
+                }
+
+            } else {
+
+                mouthAngle += 2;
+
+                if (mouthAngle >= 40) {
+                    closing = true;
+                }
+            }
         }
     }
 
@@ -87,25 +138,96 @@ public class Player extends Entity {
         double nx = x;
         double ny = y;
 
-        if (dir.equals("LEFT"))  nx -= maze.TILE_SIZE;
-        if (dir.equals("RIGHT")) nx += maze.TILE_SIZE;
-        if (dir.equals("UP"))    ny -= maze.TILE_SIZE;
-        if (dir.equals("DOWN"))  ny += maze.TILE_SIZE;
+        switch (dir) {
+
+            case "LEFT":
+                nx -= maze.TILE_SIZE;
+                break;
+
+            case "RIGHT":
+                nx += maze.TILE_SIZE;
+                break;
+
+            case "UP":
+                ny -= maze.TILE_SIZE;
+                break;
+
+            case "DOWN":
+                ny += maze.TILE_SIZE;
+                break;
+        }
 
         return collision.canMove(nx, ny, radius);
     }
 
     @Override
     public void draw(GraphicsContext gc) {
+
         gc.setFill(Color.YELLOW);
-        gc.fillOval(x - radius, y - radius, radius * 2, radius * 2);
+
+        // resting = full circle
+        if (currentDir.isEmpty()) {
+
+            gc.fillOval(
+                    x - radius,
+                    y - radius,
+                    radius * 2,
+                    radius * 2
+            );
+
+            return;
+        }
+
+        double startAngle = 0;
+        double arcLength = 360 - (mouthAngle * 2);
+
+        // rotate mouth according to direction
+        switch (currentDir) {
+
+            case "RIGHT":
+                startAngle = mouthAngle;
+                break;
+
+            case "LEFT":
+                startAngle = 180 + mouthAngle;
+                break;
+
+            case "UP":
+                startAngle = 90 + mouthAngle;
+                break;
+
+            case "DOWN":
+                startAngle = 270 + mouthAngle;
+                break;
+        }
+
+        gc.fillArc(
+                x - radius,
+                y - radius,
+                radius * 2,
+                radius * 2,
+                startAngle,
+                arcLength,
+                ArcType.ROUND
+        );
     }
 
     public void setDirection(KeyCode code) {
 
-        if (code == KeyCode.LEFT)  nextDir = "LEFT";
-        if (code == KeyCode.RIGHT) nextDir = "RIGHT";
-        if (code == KeyCode.UP)    nextDir = "UP";
-        if (code == KeyCode.DOWN)  nextDir = "DOWN";
+        if (code == KeyCode.LEFT) {
+            nextDir = "LEFT";
+        }
+
+        if (code == KeyCode.RIGHT) {
+            nextDir = "RIGHT";
+        }
+
+        if (code == KeyCode.UP) {
+            nextDir = "UP";
+        }
+
+        if (code == KeyCode.DOWN) {
+            nextDir = "DOWN";
+        }
     }
 }
